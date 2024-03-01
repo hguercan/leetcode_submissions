689a5ffd06dbedf859dfcab47a54bde59193c451
Hasan Gürcan <72599474+hguercan@users.noreply.github.com>
2024-03-01 23:54:32 +0100

    Time: 32 ms (37.96%) | Memory: 55.3 MB (16.68%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -1,38 +1,22 @@
 class MyHashMap {
 
-    private List<Integer> keyList;
-    private List<Integer> valueList;
+    private int[] keyValueArr;
 
     public MyHashMap() {
-        this.keyList = new ArrayList<>();
-        this.valueList = new ArrayList<>();
+        this.keyValueArr = new int[1000001];
+        Arrays.fill(keyValueArr, -1);
     }
     
     public void put(int key, int value) {
-        var keyOccurenceIndex = keyList.indexOf(key);
-        if (keyOccurenceIndex == -1) {
-            keyList.add(key);
-            valueList.add(value);
-        } else {
-            valueList.set(keyOccurenceIndex, value);
-        }
+        keyValueArr[key] = value;
     }
     
     public int get(int key) {
-        var retrievedValue = -1;
-        var keyOccurenceIndex = keyList.indexOf(key);
-        if (keyOccurenceIndex != -1) {
-            retrievedValue = valueList.get(keyOccurenceIndex);
-        }
-        return retrievedValue;
+        return keyValueArr[key];
     }
     
     public void remove(int key) {
-        var keyOccurenceIndex = keyList.indexOf(key);
-        if (keyOccurenceIndex != -1) {
-            keyList.remove(keyOccurenceIndex);
-            valueList.remove(keyOccurenceIndex);
-        }
+        keyValueArr[key] = -1;
     }
 }
 
