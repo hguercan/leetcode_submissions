3a702623078f8595aa99ff5e37a01ba12a203a98
Hasan Gürcan <72599474+hguercan@users.noreply.github.com>
2024-03-02 23:35:11 +0100

    Time: 30 ms (93.96%) | Memory: 16.5 MB (93.22%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,6 +3,6 @@ class Solution:
         shortest_word = min(strs, key=len)
         prefix = ""
         for j in range(1, len(shortest_word)+1):
-            candidate = shortest_word[:j]
+            candidate = shortest_word[0:j]
             prefix = candidate if all(word.startswith(candidate) for word in strs) else prefix
         return prefix
