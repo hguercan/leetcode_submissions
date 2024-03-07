38f3a0e4063abf8154726b53003a3c67edf75e3f
Hasan Gürcan <72599474+hguercan@users.noreply.github.com>
2024-03-07 16:50:09 +0100

    Time: 12 ms (100.00%) | Memory: 41.5 MB (8.39%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -12,8 +12,24 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         if (head.next == null) {
             return head;
+        } else if (head.next.next == null) {
+            return head.next;
         }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
 
+        printSlowAndFast(slow, fast);
+        while (fast != null) {
+            //printSlowAndFast(slow, fast);
+            slow = slow.next;
+            fast = getNextFast(fast);
+            printSlowAndFast(slow, fast);
+        }
+        return slow;
+
+
+        /*
         int counter = 0;
         ListNode currNode = head;
         while(currNode.next != null) {
@@ -29,5 +45,17 @@ class Solution {
             counterToMid--;
         }
         return midOfList;
+        */
+    }
+
+    private ListNode getNextFast(ListNode fast) {
+        return fast.next == null ? null : fast.next.next;
+    }
+
+    private void printSlowAndFast(ListNode slow, ListNode fast) {
+        var slowValue = slow != null ? slow.val : -1;
+        var fastValue = fast != null ? fast.val : -1;
+        var msg = String.format("Slow: %d, Fast: %d", slowValue, fastValue);
+        System.out.println(msg);
     }
 }
