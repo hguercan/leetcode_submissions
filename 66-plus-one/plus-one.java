7fc942dff8a2c168ea029bd2d729094547f63cbe
Hasan Gürcan <72599474+hguercan@users.noreply.github.com>
2024-03-08 20:49:08 +0100

    Time: 0 ms (100.00%) | Memory: 41.4 MB (90.25%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,8 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         var lastIndex = digits.length - 1;
-        var lastValue = digits[lastIndex];
-        return incrementDigit(digits, lastValue, lastIndex);
+        return incrementDigit(digits, digits[lastIndex], lastIndex);
     }
 
     private int[] incrementDigit(int[] digits, int digit, int currIndex) {
@@ -11,17 +10,14 @@ class Solution {
                 digits[currIndex] = incrementedValue % 10;
                 var nextIndex = currIndex - 1;
                 if(nextIndex == -1) {
-                    System.out.println(String.format("New Digits Length: %d", digits.length+1));
                     int[] newDigits = new int[digits.length+1];
                     for(int i=0; i < digits.length; i++) {
-                        System.out.println(String.format("Iteration i: %d", i+1));
-                        System.out.println(Arrays.toString(digits));
                         newDigits[i+1] = digits[i];
                     }
                     digits = newDigits;
                     nextIndex = 0;
                 }
-                return incrementDigit(digits, digits[nextIndex], nextIndex);
+                digits = incrementDigit(digits, digits[nextIndex], nextIndex);
         } else {
             digits[currIndex] = incrementedValue;
         }
