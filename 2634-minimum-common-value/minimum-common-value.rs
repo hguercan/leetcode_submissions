b9b10a93a5191687c1e7b290748dffd9218642e5
Hasan Gürcan <72599474+hguercan@users.noreply.github.com>
2024-03-09 11:45:21 +0100

    Time: 7 ms (80.95%) | Memory: 3.9 MB (52.38%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn get_common(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        let mut index_one: usize = 0;
+        let mut index_two: usize = 0;
+
+        while index_one < nums1.len() && index_two < nums2.len() {
+            let mut val_one = nums1[index_one];
+            let mut val_two = nums2[index_two];
+
+            if val_one == val_two {
+                return val_one;
+            } else if val_one < val_two {
+                index_one += 1;
+            } else {
+                index_two += 1;
+            }
+        }
+
+        return -1;
+    }
+}
