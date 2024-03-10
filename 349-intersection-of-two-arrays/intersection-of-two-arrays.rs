2bb30e574c5a6d2e86b7a5a6247fbc38dc40f4b4
Hasan Gürcan <72599474+hguercan@users.noreply.github.com>
2024-03-10 20:44:24 +0100

    Time: 1 ms (82.69%) | Memory: 2.1 MB (99.04%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.rs
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.rs
@@ -1,14 +1,26 @@
 use std::collections::HashSet;
 
 impl Solution {
+
+    /* Second solution */
     pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let set_nums1: HashSet<i32> = nums1.into_iter().collect();
+        let set_nums2: HashSet<i32> = nums2.into_iter().collect();
+
+        set_nums1.intersection(&set_nums2).cloned().collect()
+    }
+
+
+    /* First solution 
+    pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        
         if nums1.len() <= nums2.len() {
             Solution::findIntersection(nums1, nums2)
         } else {
             Solution::findIntersection(nums2, nums1)
         }
     }
-
+    
     pub fn findIntersection(numsShort: Vec<i32>, numsLong: Vec<i32>) -> Vec<i32> {
         let mut intersects: HashSet<i32> = HashSet::new();
         for candidate in numsShort.iter() {
@@ -18,5 +30,5 @@ impl Solution {
         }
 
         Vec::from_iter(intersects)
-    }
+    }*/
 }
